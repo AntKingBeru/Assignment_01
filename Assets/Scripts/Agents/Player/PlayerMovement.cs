feec3b58c41149df15ecbf445184b4f5e15676fc
Beru <lidor.y.shapira@gmail.com>
2026-01-20 11:41:53 +0200

    improved building system and added environment
    added mountain and forest floors and layers
    added mountain face
    added summon circle location with spawn points
    blocked movement through UI - finally!

--- Assets/Scripts/Agents/Player/PlayerMovement.cs
+++ Assets/Scripts/Agents/Player/PlayerMovement.cs
@@ -1,39 +1,58 @@
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
-{
-    [SerializeField] private NavMeshAgent agent;
-    [SerializeField] private InputActionReference targetInput;
-    [SerializeField] private Animator animator;
+ {
+     [SerializeField] private NavMeshAgent agent;
+     [SerializeField] private InputActionReference targetInput;
+     [SerializeField] private Animator animator;
+ 
+     private Camera _mainCamera;
+     private bool _moveRequested;
+     private float _speed;
+ 
+     private void Start()
+     {
+         _mainCamera = Camera.main;
+ 
+         targetInput.action.performed += OnTarget;
+     }
+     
+     private void OnDestroy()
+     {
+         targetInput.action.performed -= OnTarget;
+     }
+ 
+     private void OnTarget(InputAction.CallbackContext context)
+     {
+         _moveRequested = true;
+     }
 
-    private Camera _mainCamera;
-    private float _speed;
-
-    private void Start()
-    {
-        _mainCamera = Camera.main;
-
-        targetInput.action.performed += OnTarget;
-    }
-    
-    private void Update()
-    {
-        _speed = agent.velocity.magnitude;
-        _speed = Mathf.InverseLerp(_speed, 0f, 1f);
-        
-        animator.SetFloat("Speed", _speed);
-    }
-
-    private void OnTarget(InputAction.CallbackContext context)
-    {
-        if (BuildUIController.Instance != null && BuildUIController.Instance.IsPlacing) return;
-        
-        var ray = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-        
-        if (!Physics.Raycast(ray, out var hit)) return;
-        
-        agent.SetDestination(hit.point);
-    }
-}
+     private void Update()
+     {
+         _speed = agent.velocity.magnitude;
+         _speed = Mathf.InverseLerp(_speed, 0f, 1f);
+         
+         animator.SetFloat("Speed", _speed);
+         
+         if (!_moveRequested) return;
+         _moveRequested = false;
+         
+         if (BuildUIController.Instance && BuildUIController.Instance.IsPlacing) return;
+         
+         if (IsPointerOverUI()) return;
+         
+         var ray = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+         
+         if (!Physics.Raycast(ray, out var hit)) return;
+         
+         agent.SetDestination(hit.point);
+     }
+ 
+     private bool IsPointerOverUI()
+     {
+         return EventSystem.current != null && EventSystem.current.IsPointerOverGameObject();
+     }
+ }
